8c482d8390bd35b469816c6f9b0e3c42cee9fc32
Andy Zhang <andzhng@gmail.com>
2018-01-24 19:49:09 -0500

    something basic, needs swagger styling

--- scripts.js
+++ scripts.js
@@ -15,13 +15,51 @@ $(document).ready(() => {
       person['fun-fact'] = d[4];
       person['talk'] = d[5];
       person['coop'] = d[6];
-      return d;
+      return person;
     });
   }
 
   // Parse and render response
   function processResponse(resp) {
     let people = parseResponse(resp);
-    console.log(people);
+    renderPeople(document.getElementById('people'), people);
+  }
+
+  // Render all da people
+  function renderPeople(rootElem, people) {
+    for (let i = 0; i < people.length; i++) {
+      rootElem.appendChild(createPersonElement(people[i]));
+    }
+  }
+
+  function createPersonElement(person) {
+    let root = document.createElement('div');
+
+    let name = document.createElement('div');
+    name.className = 'name';
+    name.innerHTML = person['name'];
+
+    let term = document.createElement('div');
+    term.className = 'term';
+    term.innerHTML = person['term'];
+
+    let funFact = document.createElement('div');
+    funFact.className = 'fun-fact';
+    funFact.className = person['fun-fact'];
+
+    let talk = document.createElement('div');
+    talk.className = 'talk';
+    talk.innerHTML = person['talk'];
+
+    let coop = document.createElement('div');
+    coop.className = 'coop';
+    coop.innerHTML = person['coop'];
+
+    root.appendChild(name);
+    root.appendChild(term);
+    root.appendChild(funFact);
+    root.appendChild(talk);
+    root.appendChild(coop);
+    return root;
   }
 });
