f2ad30baa58ed23abd2b5cf510774eb48b765133
Andy Zhang <andzhng@gmail.com>
2018-01-24 19:39:09 -0500

    Replace with tab

--- scripts.js
+++ scripts.js
@@ -1,16 +1,25 @@
 $(document).ready(() => {
-  $.get('./responses.csv', processResponse);
+  $.get('./responses.tsv', processResponse);
 
+  // Go through CSV and return array of people
   function parseResponse(data) {
     let rows = data.split('\n');
     rows.shift(); // Remove column names
     return rows.map((row) => {
-      let d = row.split(',');
-      d.shift();
+      let d = row.split('\t');
+
+      let person = {};
+      person['name'] = d[1];
+      person['term'] = d[2];
+      person['contact'] = d[3];
+      person['fun-fact'] = d[4];
+      person['talk'] = d[5];
+      person['coop'] = d[6];
       return d;
     });
   }
 
+  // Parse and render response
   function processResponse(resp) {
     let people = parseResponse(resp);
     console.log(people);
