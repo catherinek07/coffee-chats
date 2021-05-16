9268a1b1d1c04ebd023544bd191cf2ed09963549
Derrek Chow <derrekdc@gmail.com>
2021-05-16 17:15:51 +0800

    2021 setup

--- scripts.js
+++ scripts.js
@@ -3,86 +3,86 @@
  * @param {Array} a items An array containing the items.
  */
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
+		var j, x, i;
+		for (i = a.length - 1; i > 0; i--) {
+				j = Math.floor(Math.random() * (i + 1));
+				x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+		}
 }
 
 $(document).ready(() => {
-  $.get('./data.tsv', processResponse);
+	$.get('./data.tsv', processResponse);
 
-  // Go through CSV and return array of people
-  function parseResponse(data) {
-    let rows = data.split('\n');
-    rows.shift(); // Remove column names
-    return rows.map((row) => {
-      let d = row.split('\t');
+	// Go through CSV and return array of people
+	function parseResponse(data) {
+		let rows = data.split('\n');
+		rows.shift(); // Remove column names
+		return rows.map((row) => {
+			let d = row.split('\t');
 
-      let person = {};
-      person['name'] = d[0];
-      person['year'] = d[1]
-	    person['about'] = d[2];
-      person['talk'] = d[3];
-      person['fun-fact'] = d[4];
-      
-      return person;
-    });
-  }
+			let person = {};
+			person['name'] = d[0];
+			//person['year'] = d[1]
+			person['about'] = d[1];
+			person['talk'] = d[2];
+			person['fun-fact'] = d[3];
+			
+			return person;
+		});
+	}
 
-  // Parse and render response
-  function processResponse(resp) {
-    let people = parseResponse(resp);
-    shuffle(people); // Mix up the order each time so no one is always at the top
-    renderPeople(document.getElementById('people'), people);
-  }
+	// Parse and render response
+	function processResponse(resp) {
+		let people = parseResponse(resp);
+		shuffle(people); // Mix up the order each time so no one is always at the top
+		renderPeople(document.getElementById('people'), people);
+	}
 
-  // Render all da people
-  function renderPeople(rootElem, people) {
-    for (let i = 0; i < people.length; i++) {
-      rootElem.appendChild(createPersonElement(people[i]));
-    }
-  }
+	// Render all da people
+	function renderPeople(rootElem, people) {
+		for (let i = 0; i < people.length; i++) {
+			rootElem.appendChild(createPersonElement(people[i]));
+		}
+	}
 
-  function createPersonElement(person) {
-    let root = document.createElement('div');
-    root.className = 'person';
+	function createPersonElement(person) {
+		let root = document.createElement('div');
+		root.className = 'person';
 
-    let aboutHeader = document.createElement('h3');
-  
-    let year = document.createElement('span');
-    year.className = 'year';
-    year.innerHTML = person['year'];
+		let aboutHeader = document.createElement('h3');
+	
+		// let year = document.createElement('span');
+		// year.className = 'year';
+		// year.innerHTML = person['year'];
 
-    if (person['year'] == 2022) {
-      year.innerHTML = '3A';
-    }
-    else if (person['year'] == 2020) {
-      year.innerHTML = '4B';
-    }
+		// if (person['year'] == 2022) {
+		// 	year.innerHTML = '3A';
+		// }
+		// else if (person['year'] == 2020) {
+		// 	year.innerHTML = '4B';
+		// }
 
-    aboutHeader.innerHTML = person['name'] + " " + year.outerHTML;
-    root.appendChild(aboutHeader);
+		//aboutHeader.innerHTML = person['name'] + " " + year.outerHTML;
+		root.appendChild(aboutHeader);
 
-	  let about = document.createElement('div');
-    about.className = 'about';
-    about.innerHTML = person['about'];
+		let about = document.createElement('div');
+		about.className = 'about';
+		about.innerHTML = person['about'];
 
-    let funFact = document.createElement('div');
-    funFact.className = 'fun-fact';
-    funFact.innerHTML = person['fun-fact'];
+		let funFact = document.createElement('div');
+		funFact.className = 'fun-fact';
+		funFact.innerHTML = person['fun-fact'];
 
-    let talk = document.createElement('div');
-    talk.className = 'talk';
-    talk.innerHTML = person['talk'];
+		let talk = document.createElement('div');
+		talk.className = 'talk';
+		talk.innerHTML = person['talk'];
 
-    root.appendChild(about);
-    root.appendChild(talk);
-    root.appendChild(funFact);
-    
-    return root;
-  }
+		root.appendChild(about);
+		root.appendChild(talk);
+		root.appendChild(funFact);
+		
+		return root;
+	}
 });
